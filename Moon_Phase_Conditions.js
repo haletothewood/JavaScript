ab40a8fe3e9cd2e80682ac5463a70a3562b8f6bc
David Halewood <davidnhalewood@hotmail.com>
2017-08-14 17:13:02 +0100

    Adding case statements

--- Moon_Phase_Conditions.js
+++ Moon_Phase_Conditions.js
@@ -1,3 +1,4 @@
+/* Using Case Statements instead
 var moonPhase = "full"
 var foggyNight = false
 
@@ -10,4 +11,24 @@ if (moonPhase === "full" || foggyNight) {
   console.log("Back on two feet");
 } else {
   console.log("Invalid moon phase");
+}
+*/
+
+//Case Statements
+
+var moonPhase = "full"
+
+switch (moonPhase) {
+  case "full":
+    console.log("Howwwwlll!!");
+    break;
+  case "mostly full":
+    console.log("Arms and legs are getting hairier");
+    break;
+  case "mostly new":
+    console.log("Back on two feet");
+    break;
+  default:
+    console.log("Invalid moon phase")
+    break;
 }
