1f0efda6a940349858da5851fbe1278841447087
David Halewood <davidnhalewood@hotmail.com>
2017-08-14 11:40:18 +0100

    Making Dog_Years.js interactive

--- Dog_Years.js
+++ Dog_Years.js
@@ -1,7 +1,17 @@
-var MyAge = 34
-var EarlyYears = (2*10.5)
-var LaterYears = (MyAge - 2) * 4
+console.log("I will now ask you for your age.");
+process.stdin;
+var Age;
+process.stdin.on('readable', function() {
+    Age = process.stdin.read();
+    if (Age !== null) {
+        console.log("Wow, " + Age + "You look great for your age!");
+        var EarlyYears = (2*10.5);
+		var LaterYears = (Age - 2) * 4;
 
-var MyAgeInDogYears = EarlyYears + LaterYears
+		var MyAgeInDogYears = EarlyYears + LaterYears;
 
-console.log(MyAgeInDogYears)
+		console.log("Did you know if you were a dog you'd be " + MyAgeInDogYears)
+		process.exit();
+    }
+
+});
