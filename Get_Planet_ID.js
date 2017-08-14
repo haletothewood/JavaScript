244a2e18ca7073176c05700d464c6c47ca58018d
David Halewood <davidnhalewood@hotmail.com>
2017-08-14 13:31:59 +0100

    Adding CodeWars Instructions

--- Get_Planet_ID.js
+++ Get_Planet_ID.js
@@ -1,4 +1,8 @@
-/*
+//The function is not returning the correct values. Can you figure out why?
+
+//getPlanetName(3); // should return 'Earth'
+
+/*Fixed
 function getPlanetName(id){
   var name;
   switch(id){
