441860f3fbb1239b93bf4ed27c0e1b7d4f669a07
David Halewood <davidnhalewood@hotmail.com>
2017-08-19 10:51:01 +0100

    adding sum total function

--- Pizza_Order.js
+++ Pizza_Order.js
@@ -1,5 +1,16 @@
+var orderCount = 0
+
 function takeOrder(crustType, topping) {
+	orderCount = orderCount + 1;
 	return('Order: ' + crustType + ' pizza topped with ' + topping);
 }
 
+function getSumTotal(itemCount) {
+	return itemCount * 7.5;
+}
+
 console.log(takeOrder('Thin crust', 'extra cheese'))
+console.log(takeOrder('Deep dish', 'meatballs'))
+console.log(takeOrder('Thin crust', 'goats cheese'))
+
+console.log(getSumTotal(orderCount))
