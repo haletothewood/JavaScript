2ef7c26d93dda0e62d6bc850a75f2d0786780fc5
David Halewood <davidnhalewood@hotmail.com>
2017-08-19 10:18:52 +0100

    adding interactivity

--- Magic_Eight_Ball.js
+++ Magic_Eight_Ball.js
@@ -1,34 +1,42 @@
-var userQuestion = "Will I ever win the lottery?";
+console.log("What would you like to ask the Eight Ball?");
+process.stdin;
+var userQuestion;
+process.stdin.on('readable', function() {
+    userQuestion = process.stdin.read();
+    if (userQuestion !== null) {
+    var randomNumber = Math.floor(Math.random()*7);
+    var eightBall = ""
 
-var randomNumber = Math.floor(Math.random()*7);
+      switch (randomNumber) {
+        case 0:
+          eightBall = "It is certain";
+  	      break;
+        case 1:
+          eightBall = "It is decidedly so";
+          break;
+        case 2:
+          eightBall = "Reply hazy, try again";
+          break;
+        case 3:
+          eightBall = "Cannot predict now";
+          break;
+        case 4:
+          eightBall = "Don't count on it";
+          break;
+        case 5: 
+          eightBall = "My sources say no";
+          break;
+        case 6:
+          eightBall = "Outlook not so good";
+          break;
+        case 7: 
+          eightBall = "Signs point to yes";
+          break;
+      }
+      console.log(eightBall)
+      process.exit();
+    }
 
-var eightBall = ""
+})
 
-switch (randomNumber) {
-  case 0:
-    eightBall = "It is certain";
-  	break;
-  case 1:
-    eightBall = "It is decidedly so";
-    break;
-  case 2:
-    eightBall = "Reply hazy, try again";
-    break;
-  case 3:
-    eightBall = "Cannot predict now";
-    break;
-  case 4:
-    eightBall = "Don't count on it";
-    break;
-  case 5: 
-    eightBall = "My sources say no";
-    break;
-  case 6:
-    eightBall = "Outlook not so good";
-    break;
-  case 7: 
-    eightBall = "Signs point to yes";
-    break;
-}
 
-console.log(eightBall)
