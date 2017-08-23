da7679cfccf86080b4941a7456a22030246dff7f
David Halewood <davidnhalewood@hotmail.com>
2017-08-23 15:10:31 +0100

    cleaning up syntax

--- RPS.js
+++ RPS.js
@@ -10,37 +10,38 @@ function getComputerChoice() {
       return 'paper';
     case 2:
       return 'scissors';
-    }
+  }
 }
 
 function determineWinner(userChoice, computerChoice) {
   if (userChoice === computerChoice) {
     return "The game was a tie"; 
-  }  if (userChoice === 'rock') {
-				if (computerChoice === 'paper') {
-    			return 'The computer won!';
- 				} else {
-    			return 'You won!';
+  }  
+  if (userChoice === 'rock') {
+		if (computerChoice === 'paper') {
+    	return 'The computer won!';
+ 		} else {
+    	return 'You won!';
+    }
   }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
   }
-if (userChoice === 'paper') {
-  if (computerChoice === 'scissors') {
-    return 'The computer won!';
-  } else {
-    return 'You won!';
-  }
-}
   if (userChoice === 'scissors') {
-  if (computerChoice === 'rock') {
-    return 'The computer won!';
-  } else {
-    return 'You won!';
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
   }
-}
   if (userChoice === 'bomb'){
     return 'Game over, user wins!'
   }
-  }
+}
 
 function playGame() {
 var userChoice = userInput
