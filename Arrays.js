6b27e005befa1e940364f63b1550b98be04ae316
David Halewood <davidnhalewood@hotmail.com>
2017-08-29 15:27:16 +0100

    adding comments

--- Arrays.js
+++ Arrays.js
@@ -6,14 +6,14 @@ console.log('__')
 console.log("If we use the push function we can add to the end of the array")
 console.log('__')
 
-arr.push(6, 7)
+arr.push(6, 7) //pushes 6 & 7 to the end of the array
 
 console.log("The array is now: " + arr)
 console.log('__')
 console.log("If we use the pop function we can remove the end item")
 console.log('__')
 
-arr.pop()
+arr.pop() //pops off the end item from the array
 
 console.log("Which leaves the array like this: " + arr)
 console.log('__')
@@ -26,8 +26,11 @@ console.log('__')
 
 console.log("We can convert the arr to a string using arr.toString()")
 console.log('__')
-console.log(arr.toString())
+
+console.log(arr.toString()) //converts to string with commas no spaces
+
 console.log('__')
 console.log("Or we can use arr.join(', ') which allows us to use a separator")
 console.log('__')
-console.log(arr.join(', '))
+
+console.log(arr.join(', ')) //behaves in same way as toString with separator
